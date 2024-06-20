21e2e8e6cfd53065be10bce075b53574fbe168a8
maragonishiva25 <maragonishiva25@Maragoni>
2024-06-20 12:55:48 +0530

    initial commit

---
+++ src/test/java/shivaGithub/Test2.java
@@ -0,0 +1,14 @@
+package shivaGithub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2
+{
+	@Test
+	public void sampletest() {
+		Reporter.log("test1-sampletest",true);
+	}
+	
+
+}
